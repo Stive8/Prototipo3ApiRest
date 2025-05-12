8ab4a1f1f01680a1fcfe7442976291fbe645da08
Brayhan Ortegon <bsortegonbarrios@gmail.com>
2025-05-12 17:03:08 -0500

    Creacion Spring Servidor

---
+++ Servidor/src/main/java/acueducto/empresariales/GestorDeAcueductoApplication.java
@@ -0,0 +1,13 @@
+package acueducto.empresariales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeAcueductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeAcueductoApplication.class, args);
+	}
+
+}
---
+++ Servidor/src/test/java/acueducto/empresariales/GestorDeAcueductoApplicationTests.java
@@ -0,0 +1,13 @@
+package acueducto.empresariales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeAcueductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
